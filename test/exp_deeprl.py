71fe9bf0f2ab46bd8ef2f4c7f1fdb9fd552f1451
Yu Lou <louyu27@gmail.com>
2017-07-06 15:32:28 +0800

    2017.6.6 14:47 Experiment(Pong)

--- test/exp_deeprl.py
+++ test/exp_deeprl.py
@@ -1084,7 +1084,7 @@ class BootstrappedDQNAtari(Experiment):
                                 loss_function=self.loss_function,
                                 trainer=tf.train.GradientDescentOptimizer(learning_rate=0.001).minimize,
                                 replay_buffer_class=hrl.playback.MapPlayback,
-                                replay_buffer_args={"capacity": 10000},
+                                replay_buffer_args={"capacity": 50000},
                                 min_buffer_size=5000,
                                 batch_size=8,
                                 n_heads=n_head)
