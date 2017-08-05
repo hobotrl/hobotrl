7f43c6bbe786e641028f36ea08670d532d5594d2
Lisen Mu <immars@gmail.com>
2017-08-05 16:00:22 +0800

    async dqn experiment added

---
+++ scripts/adqn.sh
@@ -0,0 +1,21 @@
+#
+#	usage: . ./scripts/a3c_pong.sh
+#
+
+exp_file=./test/exp_new.py
+exp_name=ADQNExperiment
+log_dir=./log/$exp_name
+device=2
+# cluster="{'ps':['localhost:2232'], 'worker':['localhost:2233', 'localhost:2234', 'localhost:2235']}"
+cluster="{'ps':['localhost:2232'], 'worker':['localhost:2233','localhost:2234']}"
+worker_n=2
+
+mkdir -p $log_dir 
+python $exp_file run --name $exp_name --cluster "$cluster" --job ps > $log_dir/ps.txt 2>&1 &
+end_i=$(expr $worker_n - 1)
+echo "job $exp_name with worker: [ 0 .. $end_i ]"
+for i in $(seq 0 $end_i)
+do
+	CUDA_VISIBLE_DEVICES=$device python $exp_file run --name $exp_name --cluster "$cluster" --job worker --index $i > $log_dir/worker.$i.txt 2>&1 &
+done
+
--- test/exp_new.py
+++ test/exp_new.py
@@ -14,7 +14,7 @@ from hobotrl.experiment import Experiment
 from hobotrl.cluster import ClusterAgent
 import hobotrl.network as network
 import hobotrl.algorithms.ac_c as ac_c
-
+import hobotrl.algorithms.dqn_c as dqn_c
 
 class ACExperiment(Experiment):
     def run(self, args):
@@ -24,6 +24,7 @@ class ACExperiment(Experiment):
         state_shape = list(env.observation_space.shape)
 
         discount_factor = 0.9
+
         def f_net(inputs):
             l2 = 1e-4
             state = inputs[0]
@@ -46,7 +47,7 @@ class ACExperiment(Experiment):
                 target_estimator=None,
                 max_advantage=100.0,
                 # optimizer arguments
-                network_optmizer=network.LocalOptimizer(tf.train.AdamOptimizer(1e-3), grad_clip=10.0),
+                network_optmizer=n_optimizer,
                 max_gradient=10.0,
                 # sampler arguments
                 sampler=None,
@@ -66,6 +67,54 @@ class ACExperiment(Experiment):
             runner.episode(1000)
 
 Experiment.register(ACExperiment, "test A3C")
+
+
+class ADQNExperiment(Experiment):
+    def run(self, args):
+        env = gym.make('Pendulum-v0')
+        env = hrl.envs.C2DEnvWrapper(env, [5])
+        env = hrl.envs.ScaledRewards(env, 0.1)
+        state_shape = list(env.observation_space.shape)
+
+        discount_factor = 0.9
+
+        def f_q(inputs):
+            q = network.Utils.layer_fcs(inputs[0], [200, 100], env.action_space.n, l2=1e-4)
+            return {"q": q}
+
+        def create_optimizer():
+            return tf.train.AdamOptimizer(1e-3)
+
+        def create_agent(n_optimizer, global_step):
+            agent = dqn_c.DQN(
+                f_create_q=f_q,
+                state_shape=state_shape,
+                # OneStepTD arguments
+                num_actions=env.action_space.n,
+                discount_factor=0.99,
+                ddqn=False,
+                # target network sync arguments
+                target_sync_interval=100,
+                target_sync_rate=1.0,
+                # sampler arguments
+                update_interval=4, replay_size=1000, batch_size=32,
+                # epsilon greedy arguments
+                greedy_epsilon=hrl.utils.CappedLinear(1e5, 0.5, 0.1),
+                global_step=global_step,
+                network_optmizer=n_optimizer
+            )
+            return agent
+
+        agent = ClusterAgent(create_agent, create_optimizer, args.cluster, args.job, args.index, args.logdir)
+        with agent.wait_for_session() as sess:
+            agent.set_session(sess)
+            runner = hrl.envs.EnvRunner(env, agent, reward_decay=discount_factor,
+                                        evaluate_interval=sys.maxint, render_interval=sys.maxint,
+                                        render_once=True,
+                                        logdir=args.logdir if args.index == 0 else None)
+            runner.episode(1000)
+
+Experiment.register(ADQNExperiment, "test ADQN")
 
 if __name__ == '__main__':
     Experiment.main()
