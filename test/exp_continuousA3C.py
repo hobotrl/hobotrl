3514d8deaf6aa8de97823e0c7946e0cc3886f855
RuihongQiu <qiuruihong@foxmail.com>
2017-08-22 09:52:25 +0800

    Share parameters in CarRacing network.

--- test/exp_continuousA3C.py
+++ test/exp_continuousA3C.py
@@ -85,7 +85,7 @@ class A3CCarExp(ACOOExperimentCon):
     def __init__(self, env, f_create_net=None,
                  episode_n=10000,
                  reward_decay=0.99,
-                 entropy_scale=0.0001,
+                 entropy_scale=0.05,
                  on_batch_size=32,
                  off_batch_size=32,
                  off_interval=0,
@@ -94,48 +94,42 @@ class A3CCarExp(ACOOExperimentCon):
                  prob_min=5e-3,
                  entropy=hrl.utils.CappedLinear(1e6, 1e-2, 1e-3),
                  l2=1e-8,
-                 optimizer_ctor=lambda: tf.train.AdamOptimizer(1e-4), ddqn=False, aux_r=False, aux_d=False):
+                 optimizer_ctor=lambda: tf.train.AdamOptimizer(6e-5), ddqn=False, aux_r=False, aux_d=False):
 
         def create_ac_car(input_state, num_action, **kwargs):
-            se_v = hrl.utils.Network.conv2ds(input_state,
+            se = hrl.utils.Network.conv2ds(input_state,
                                            shape=[(32, 8, 4), (64, 4, 2), (64, 3, 1)],
                                            out_flatten=True,
                                            activation=tf.nn.relu,
                                            l2=l2,
-                                           var_scope="se_v")
+                                           var_scope="se")
 
-            se_pi = hrl.utils.Network.conv2ds(input_state,
-                                             shape=[(32, 8, 4), (64, 4, 2), (64, 3, 1)],
-                                             out_flatten=True,
-                                             activation=tf.nn.relu,
-                                             l2=l2,
-                                             var_scope="se_pi")
 
-            v = hrl.utils.Network.layer_fcs(se_v, [256], 1,
+            v = hrl.utils.Network.layer_fcs(se, [256], 1,
                                             activation_hidden=tf.nn.relu,
                                             l2=l2,
                                             var_scope="v")
             v = tf.squeeze(v, axis=1)
 
-            pi_mean = hrl.utils.Network.layer_fcs(se_pi, [256], num_action,
+            pi_mean = hrl.utils.Network.layer_fcs(se, [256], num_action,
                                              activation_hidden=tf.nn.relu,
                                              activation_out=tf.nn.tanh,
                                              l2=l2,
                                              var_scope="pi_mean")
 
-            pi_stddev = hrl.utils.Network.layer_fcs(se_pi, [256], 1,
+            pi_stddev = hrl.utils.Network.layer_fcs(se, [256], 1,
                                                   activation_hidden=tf.nn.relu,
                                                   activation_out=tf.nn.softplus,
                                                   l2=l2,
                                                   var_scope="pi_stddev")
 
-            r = hrl.utils.Network.layer_fcs(se_v, [256], 1,
+            r = hrl.utils.Network.layer_fcs(se, [256], 1,
                                             activation_hidden=tf.nn.relu,
                                             l2=l2,
                                             var_scope="r")
             r = tf.squeeze(r, axis=1)
 
-            return {"pi_mean": pi_mean, "pi_stddev": pi_stddev, "v": v, "se_v": se_v, "se_pi": se_pi, "r": r}
+            return {"pi_mean": pi_mean, "pi_stddev": pi_stddev, "v": v, "se": se, "r": r}
         if f_create_net is None:
             f_create_net = create_ac_car
         logging.warning("before super(A3CCarExp, self).__init__")
