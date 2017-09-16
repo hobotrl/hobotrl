c5ecee8c60cf1ff5525babc31142f43b464da625
RuihongQiu <qiuruihong@foxmail.com>
2017-09-16 18:16:56 +0800

    Add original A3C test on Acrobot.

--- test/exp_icm.py
+++ test/exp_icm.py
@@ -122,5 +122,46 @@ class ICMLinear(A3CExperimentWithICM):
 Experiment.register(ICMLinear, "A3C with ICM for simple observation state experiments")
 
 
+class A3C(A3CExperiment):
+     def __init__(self, env=None, f_create_net=None, episode_n=50000,
+                  learning_rate=5e-5, discount_factor=0.99, entropy=hrl.utils.CappedLinear(1e6, 1e-4, 1e-4),
+                  batch_size=32):
+         if env is None:
+             env = gym.make('Acrobot-v1')
+             env = gym.wrappers.Monitor(env, "/home/qrh/hobotrl/log/ICM/AcrobotOriginA3C/")
+
+         if f_create_net is None:
+             dim_action = env.action_space.n
+
+             def create_ac(inputs):
+                 l2 = 1e-7
+                 input_state = inputs[0]
+
+                 se = hrl.utils.Network.layer_fcs(input_state, [200], 200,
+                                                  activation_hidden=tf.nn.relu,
+                                                  activation_out=tf.nn.relu,
+                                                  l2=l2,
+                                                  var_scope="se")
+                 v = hrl.utils.Network.layer_fcs(se, [256], 1,
+                                                 activation_hidden=tf.nn.relu,
+                                                 l2=l2,
+                                                 var_scope="v")
+                 v = tf.squeeze(v, axis=1)
+                 pi = hrl.utils.Network.layer_fcs(se, [256], dim_action,
+                                                  activation_hidden=tf.nn.relu,
+                                                  activation_out=tf.nn.softmax,
+                                                  l2=l2,
+                                                  var_scope="pi")
+
+                 return {"v": v, "pi": pi}
+
+             f_create_net = create_ac
+
+         super(A3C, self).__init__(env, f_create_net, episode_n, learning_rate, discount_factor, entropy, batch_size)
+
+
+Experiment.register(A3C, "A3C without ICM for simple observation state experiments")
+
+
 if __name__ == '__main__':
     Experiment.main()
