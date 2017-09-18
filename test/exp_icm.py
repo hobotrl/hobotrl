3f6f06d13f2b3ff4b684f3d6ef6f994e56fd3f2c
ruihong.qiu <ruihong.qiu@yz-gpu029.hogpu.cc>
2017-09-18 15:41:19 +0800

    No more wrapper needed.

--- test/exp_icm.py
+++ test/exp_icm.py
@@ -10,65 +10,6 @@ from exp_algorithms import *
 import hobotrl.environments as envs
 
 
-class RewardSparseCartPole(gym.Wrapper):
-
-    def __init__(self, env):
-        self.env = env
-        super(RewardSparseCartPole, self).__init__(env)
-        self.env.observation_space.high[2] = 0.1 * math.pi / 360
-        self.env.observation_space.low[2] = -0.1 * math.pi / 360
-        # self.step_count = 0
-
-    def _step(self, action):
-        observation, reward, done, info = self.env.step(action)
-        # if not -0.1 * math.pi / 360 < observation[2] < 0.1 * math.pi / 360:
-        #     reward = 0
-        # print "-----------------reward: ", reward
-        return observation, reward, done, info
-
-
-class RewardSparseAcrobot(gym.Wrapper):
-
-    def __init__(self, env):
-        self.env = env
-        super(RewardSparseAcrobot ,self).__init__(env)
-
-    def _step(self, action):
-        observation, reward, done, info = self.env.step(action)
-        reward = 1.0 if done else 0.0
-
-        return observation, reward, done, info
-
-
-class BalanceRewardAcrobot(gym.Wrapper):
-    def __init__(self, env):
-        self.env = env
-        super(BalanceRewardAcrobot, self).__init__(env)
-
-    def _step(self, action):
-        observation, reward, done, info = self.env.step(action)
-        self.rewardx = (-np.cos(observation[0]) - np.cos(observation[1] + observation[0]))  ##Swung height is calculated
-        if self.rewardx < .5:
-            reward = -1.
-            self.steps_beyond_done = 0
-        if (self.rewardx > .5 and self.rewardx < .8):
-            reward = -0.8
-            self.steps_beyond_done = 0
-        if self.rewardx > .8:
-            reward = -0.6
-        if self.rewardx > 1:
-            reward = -0.4
-            self.steps_beyond_done += 1
-        if self.steps_beyond_done > 4:
-            reward = -0.2
-        if self.steps_beyond_done > 8:
-            reward = -0.1
-        if self.steps_beyond_done > 12:
-            reward = 0.
-
-        return observation, reward, self.steps_beyond_done > 20, info
-
-
 class ICMLinear(A3CExperimentWithICM):
     def __init__(self, env=None, f_se=None, f_ac=None, f_forward=None, f_inverse=None, episode_n=10000,
                  learning_rate=5e-5, discount_factor=0.99, entropy=hrl.utils.CappedLinear(1e6, 1e-4, 1e-4),
@@ -76,7 +17,6 @@ class ICMLinear(A3CExperimentWithICM):
         if env is None:
             env = gym.make('Acrobot-v1')
             env = BalanceRewardAcrobot(env)
-            env = envs.ScaledRewards(env, 0.1)
             # env = gym.wrappers.Monitor(env, "/mnt/d/hobotrl/log/Acrobot/AcrobotNewWithICM/")
 
         if (f_forward and f_se and f_inverse and f_ac) is None:
