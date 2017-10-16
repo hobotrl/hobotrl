685822bbf83201d0a09c914c33fc31da6748e34e
Lisen Mu <lisen.mu@hobot.cc>
2017-10-16 09:50:39 +0800

    script refactory: reusable cluster.sh launching tensorflow cluster

--- scripts/icm.sh
+++ scripts/icm.sh
@@ -1,21 +1,8 @@
+#!/usr/bin/env bash
 #
 #	usage: . ./scripts/icm.sh
 #
 
-exp_file=./test/exp_icm.py
-exp_name=ICMLinear
-log_dir=./log/$exp_name
-# cluster="{'ps':['localhost:2232'], 'worker':['localhost:2233', 'localhost:2234', 'localhost:2235']}"
-cluster="{'ps':['localhost:2242'], 'worker':['localhost:2243', 'localhost:2244', 'localhost:2245', 'localhost:2246']}"
-worker_n=4
-device_n=4
-mkdir -p $log_dir
-python $exp_file run --name $exp_name --cluster "$cluster" --job ps --logdir $log_dir > $log_dir/ps.txt 2>&1 &
-end_i=$(expr $worker_n - 1)
-echo "job $exp_name with worker: [ 0 .. $end_i ]"
-for i in $(seq 0 $end_i)
-do
-	device=$(expr $i % $device_n)
-	CUDA_VISIBLE_DEVICES=$device python $exp_file run --name $exp_name --cluster "$cluster" --job worker --index $i --logdir $log_dir > $log_dir/worker.$i.txt 2>&1 &
-done
+. ./scripts/cluster.sh ./test/exp_icm.py ICMLinear 4 4
+
 
