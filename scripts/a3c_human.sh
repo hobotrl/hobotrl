a1055548ad91e414a9356685d4dca6e6edb516a0
Lisen Mu <lisen.mu@hobot.cc>
2017-10-23 09:54:15 +0800

    script refactory

--- scripts/a3c_human.sh
+++ scripts/a3c_human.sh
@@ -3,6 +3,6 @@
 #	usage: . ./scripts/a3c_concar.sh
 #
 
-. ./scripts/cluster.sh ./test/exp_robot.py A3CHumanoidContinuous 8 4
+. ./scripts/cluster.sh ./test/exp_robot.py A3CHumanoidContinuous 12 0 --render_once false --render_interval -1
 
 
