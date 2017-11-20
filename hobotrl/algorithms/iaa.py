6bba53b8c5a5c48fba737fc8bf09cb7c5d275076
RuihongQiu <qiuruihong@foxmail.com>
2017-11-20 16:56:36 +0800

    Logging and scripts changes.

--- hobotrl/algorithms/iaa.py
+++ hobotrl/algorithms/iaa.py
@@ -252,7 +252,7 @@ class EnvModelUpdater(network.NetworkUpdater):
                       "reward_loss": self._reward_loss,
                       "observation_loss": self._env_loss,
                       "transition_loss": self._transition_loss}
-        if self.imshow_count % 500 == 0:
+        if self.imshow_count % 1000 == 0:
             fetch_dict["s0"] = self._s0
             for i in range(self._depth):
                 fetch_dict.update({
@@ -314,18 +314,6 @@ class ActorCriticWithI2A(sampling.TrajectoryBatchUpdate,
             net_se = network.Network([input_observation], f_se, var_scope="se_1")
             se = network.NetworkFunction(net_se["se"]).output().op
 
-            # se_1 = network.Network([input_observation], f_se_1, var_scope="se_1")
-            # se_1 = network.NetworkFunction(se_1["se_1"]).output().op
-            #
-            # se_2 = network.Network([se_1], f_se_2, var_scope="se_2")
-            # se_2 = network.NetworkFunction(se_2["se_2"]).output().op
-            #
-            # se_3 = network.Network([se_2], f_se_3, var_scope="se_3")
-            # se_3 = network.NetworkFunction(se_3["se_3"]).output().op
-            #
-            # se_4 = network.Network([se_3], f_se_4, var_scope="se_4")
-            # se_4 = network.NetworkFunction(se_4["se_4"]).output().op
-
             input_reward = tf.placeholder(dtype=tf.float32, shape=[None, 3], name="input_reward")
             encode_state = tf.placeholder(dtype=tf.float32, shape=[None, state_shape[0], state_shape[1], 9], name="encode_states")
             input_frame = tf.placeholder(dtype=tf.float32, shape=[None, state_shape[0], state_shape[1], 3], name="input_frame")
@@ -375,11 +363,6 @@ class ActorCriticWithI2A(sampling.TrajectoryBatchUpdate,
                                                   input_observation[:,:,:,9:12]], axis=0)
 
             # decoder = decoder([out_next_state_concat, input_se0_concat, input_observation_concat], name_scope="decoder")
-            decoder = net_decoder([out_next_goal_concat, input_observation_concat], name_scope="decoder")
-            out_frame = network.NetworkFunction(decoder["next_frame"]).output().op
-
-            out_reward_concat = tf.concat([out_reward, out_reward1, out_reward2], axis=0)
-
             # out_action = rollout_action_function.output().op
             # out_next_state = next_state
             # out_reward = reward
@@ -430,14 +413,11 @@ class ActorCriticWithI2A(sampling.TrajectoryBatchUpdate,
             v = network.NetworkFunction(ac["v"]).output().op
             pi_dist = network.NetworkFunction(ac["pi"]).output().op
 
-            return {"v": v, "pi": pi_dist, "rollout_action": None,
-                    "next_frame": out_frame, "reward": out_reward_concat}, \
+            return {"v": v, "pi": pi_dist, "rollout_action": None,}, \
                     {
                         "se": net_se, "transition": net_model,
                         "state_decoder": net_decoder
                     }
-                    # "next_frame1": out_frame1, "reward1": out_reward1,
-                    # "next_frame2": out_frame2, "reward2": out_reward2}
         self._log_dir = log_dir
         self._rollout_depth = rollout_depth
         kwargs.update({
@@ -538,10 +518,7 @@ class ActorCriticWithI2A(sampling.TrajectoryBatchUpdate,
 
     def update_on_trajectory(self, batch):
         # self.network_optimizer.update("policy_net", self.sess, batch)
-        print "------------------------"
-        print np.shape(batch["action"])[0]
         if (np.shape(batch["action"])[0] >= self._rollout_depth):
-            print "get in"
             self.network_optimizer.update("env_model", self.sess, batch)
             self.network_optimizer.update("ac", self.sess, batch)
             self.network_optimizer.update("l2", self.sess)
@@ -574,8 +551,6 @@ class ActorCriticWithI2A(sampling.TrajectoryBatchUpdate,
                     del info[prefix + "f%d" % d], info[prefix + "f%d_predict" % d]
             return info, {}
         else:
-            print "get out"
-
             return {}, {}
 
     def set_session(self, sess):
