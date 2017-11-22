9d08f0917ab657715f798f0ffe188beacfb624f9
Lisen Mu <lisen.mu@hobot.cc>
2017-11-22 12:01:45 +0800

    k8s client modification

--- hobotrl/environments/kubernetes/client.py
+++ hobotrl/environments/kubernetes/client.py
@@ -24,21 +24,22 @@ class RemoteEnvClient(object):
 
 
 class KubernetesEnv(object):
-    def __init__(self, remote_env_class, api_server_address="train078.hogpu.cc:30794"):
+    def __init__(self, remote_client_env_class, api_server_address="train078.hogpu.cc:30794", **kwargs):
         """
 
-        :param remote_env_class: remote environment class, with constructor signature:
+        :param remote_client_env_class: remote environment class, with constructor signature:
             __init__(self, address, port, **kwargs)
 
         :param api_server_address: caller should pass api server address to overwrite
                 default server address.
         """
         super(KubernetesEnv, self).__init__()
-        self._remote_env_class = remote_env_class
+        self._remote_env_class = remote_client_env_class
         self._api_server_address = api_server_address
+        self._cls_kwargs = kwargs
         self._env = None
         self._init_env_queue = Queue.Queue(maxsize=1)
-        self._api_thread = ApiThread(remote_env_class, api_server_address, self._init_env_queue)
+        self._api_thread = ApiThread(remote_client_env_class, kwargs, api_server_address, self._init_env_queue)
         self._api_thread.start()
 
     def reset(self):
@@ -62,11 +63,12 @@ class ApiThread(threading.Thread):
 
     PING_INTERVAL = 60
 
-    def __init__(self, remote_env_class, api_server_address, env_queue,
+    def __init__(self, remote_client_env_class, cls_kwargs, api_server_address, env_queue,
                  group=None, target=None, name=None, args=(), kwargs=None, verbose=None):
         super(ApiThread, self).__init__(group, target, name, args, kwargs, verbose)
         self._env_queue = env_queue
-        self._remote_env_class = remote_env_class
+        self._remote_env_class = remote_client_env_class
+        self._cls_kwargs = cls_kwargs
         self._api_server_address = "http://" + api_server_address if api_server_address.find("http://") !=0 \
             else api_server_address
         self._stopped = False
@@ -79,7 +81,7 @@ class ApiThread(threading.Thread):
             response = http.request(self._api_server_address + "/spawn")[1]
             env_spec = json.loads(response)
             env_id = env_spec["id"]
-            self._env_queue.put(self._remote_env_class(env_spec["host"], env_spec["port"]))
+            self._env_queue.put(self._remote_env_class(env_spec["host"], env_spec["port"], **self._cls_kwargs))
         except RuntimeError, e:
             logging.warning("error creating env: %s", e)
             self._env_queue.put(e)
