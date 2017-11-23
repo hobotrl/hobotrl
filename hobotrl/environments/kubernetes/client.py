d11b3ae7908adebbe0c4229755c700746db59144
Lisen Mu <lisen.mu@hobot.cc>
2017-11-23 16:10:45 +0800

    refactory k8s client as wrapper

--- hobotrl/environments/kubernetes/client.py
+++ hobotrl/environments/kubernetes/client.py
@@ -9,6 +9,8 @@ import httplib2
 import urllib
 import json
 
+import wrapt
+
 
 class RemoteEnvClient(object):
     def __init__(self, address, port, **kwargs):
@@ -24,7 +26,7 @@ class RemoteEnvClient(object):
         pass
 
 
-class KubernetesEnv(object):
+class KubernetesEnv(wrapt.ObjectProxy):
     def __init__(self, remote_client_env_class,
                  api_server_address="train078.hogpu.cc:30794",
                  image_uri=None,
@@ -37,111 +39,62 @@ class KubernetesEnv(object):
         :param api_server_address: caller should pass api server address to overwrite
                 default server address.
         """
-        super(KubernetesEnv, self).__init__()
-        self._remote_env_class = remote_client_env_class
-        self._api_server_address = api_server_address
+        remote_env_class = remote_client_env_class
+        api_server_address = "http://" + api_server_address if api_server_address.find("http://") !=0 \
+            else api_server_address
 
-        self._cls_kwargs = kwargs
-        self._env, self._env_spec = None, None
-        self._os, self._as = None, None
-        self._init_env_queue = Queue.Queue(maxsize=1)
-        self._api_thread = ApiThread(remote_client_env_class, kwargs, api_server_address, image_uri, self._init_env_queue)
+        http = httplib2.Http()
+        if image_uri is not None:
+            url = api_server_address + "/spawn/" + image_uri
+        else:
+            url = api_server_address + "/spawn"
+
+        response = http.request(url)[1]
+        env_spec = json.loads(response)
+        logging.warning("remote env spec:%s", env_spec)
+        env_id = env_spec["id"]
+        port = env_spec["port"]
+        if isinstance(port, dict):
+            # multiple port available; choose websocket port
+            port = port["websocket"]
+        env = remote_env_class(env_spec["host"], port, **kwargs)
+        super(KubernetesEnv, self).__init__(env)
+        self._env_spec = env_spec
+        self._api_thread = ApiThread(api_server_address, env_id)
         self._api_thread.start()
 
-    def reset(self):
-        if self._env is None:
-            self._env_spec = self._init_env_queue.get()
-            if isinstance(self._env_spec, RuntimeError):
-                logging.warning("environment creation failed")
-                raise self._env
-            elif isinstance(self._env_spec, dict):
-                self._env = self._env_spec["env"]
-                del self._env_spec["env"]
-            else:
-                self._env = self._env_spec
-        return self._env.reset()
-
-    def step(self, action):
-        return self._env.step(action)
-
     def exit(self):
-        result = self._env.exit()
+        result = self.__wrapped__.exit()
         self._api_thread.stop()
         return result
-
-    @property
-    def action_space(self):
-        return self._env.action_space if self._as is None else self._as
-
-    @action_space.setter
-    def action_space(self, action_space):
-        if self._env is None:
-            self._as = action_space
-        else:
-            self._env.action_space = action_space
-
-    @property
-    def observation_space(self):
-        return self._env.observation_space if self._os is None else self._os
-
-    @observation_space.setter
-    def observation_space(self, ob):
-        if self._env is None:
-            self._os = ob
-        else:
-            self._env.observation_space = ob
 
 
 class ApiThread(threading.Thread):
 
     PING_INTERVAL = 60
 
-    def __init__(self, remote_client_env_class, cls_kwargs, api_server_address, image_uri, env_queue,
+    def __init__(self, api_server_address, env_id,
                  group=None, target=None, name=None, args=(), kwargs=None, verbose=None):
         super(ApiThread, self).__init__(group, target, name, args, kwargs, verbose)
         self.daemon = True
-        self._env_queue = env_queue
-        self._remote_env_class = remote_client_env_class
-        self._cls_kwargs = cls_kwargs
         self._api_server_address = "http://" + api_server_address if api_server_address.find("http://") !=0 \
             else api_server_address
-        self._image_uri = image_uri
+        self._env_id = env_id
         self._stopped = False
         self._sleeper = threading.Event()
 
     def run(self):
-        # init env
-        try:
-            http = httplib2.Http()
-            if self._image_uri is not None:
-                url = self._api_server_address + "/spawn/" + self._image_uri
-            else:
-                url = self._api_server_address + "/spawn"
-            response = http.request(url)[1]
-            env_spec = json.loads(response)
-            logging.warning("remote env spec:%s", env_spec)
-            env_id = env_spec["id"]
-            port = env_spec["port"]
-            if isinstance(port, dict):
-                # multiple port available; choose websocket port
-                port = port["websocket"]
-            env_spec["env"] = self._remote_env_class(env_spec["host"], port, **self._cls_kwargs)
-            self._env_queue.put(env_spec)
-        except RuntimeError, e:
-            logging.warning("error creating env: %s", e)
-            self._env_queue.put(e)
-            self._stopped = True
-            return
+        http = httplib2.Http()
         # keep alive
         while not self._stopped:
+            self._sleeper.wait(self.PING_INTERVAL)
             try:
-                http.request(self._api_server_address + "/ping/" + env_id)
+                http.request(self._api_server_address + "/ping/" + self._env_id)
             except Exception, e:
                 logging.warning("error when keeping alive:%s", e)
                 pass
-            self._sleeper.wait(self.PING_INTERVAL)
         # terminate environment
-        http.request(self._api_server_address + "/stop/" + env_id)
+        http.request(self._api_server_address + "/stop/" + self._env_id)
 
     def stop(self):
         self._stopped = True
