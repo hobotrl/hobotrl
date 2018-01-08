23354333dbd2fd66188826790818a93deae4f09a
RuihongQiu <qiuruihong@foxmail.com>
2018-01-08 11:14:39 +0800

    Script for Freeway_A3C.

--- scripts/freeway.sh
+++ scripts/freeway.sh
@@ -2,5 +2,5 @@
 #
 #	usage: . ./scripts/iaa.sh
 #
-exp_name="Freeway_A3C_half"
-. ./scripts/cluster.sh ./test/exp_freeway.py ${exp_name} 2 1 --log_dir ~/hobotrl/log/Freeway/${exp_name} --start_port 2365 --start_device 3
+exp_name="Freeway_A3C"
+. ./scripts/cluster.sh ./test/exp_freeway.py ${exp_name} 2 1 --log_dir ~/hobotrl/log/Freeway/${exp_name} --start_port 2375 --start_device 3
