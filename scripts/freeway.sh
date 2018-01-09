e680613746984c77d68e742e45ba9f1207280f2b
RuihongQiu <qiuruihong@foxmail.com>
2018-01-09 20:10:59 +0800

    exp on half input Freeway A3C

--- scripts/freeway.sh
+++ scripts/freeway.sh
@@ -2,8 +2,5 @@
 #
 #	usage: . ./scripts/iaa.sh
 #
-exp_name1="Freeway_mom_half"
-. ./scripts/cluster.sh ./test/exp_freeway.py ${exp_name1} 2 2 --log_dir ~/hobotrl/log/Freeway/${exp_name1} --start_port 2305 --start_device 0
-
-#exp_name3="Freeway_mom_I2A_half"
-#. ./scripts/cluster.sh ./test/exp_freeway.py ${exp_name3} 2 1 --log_dir ~/hobotrl/log/Freeway/${exp_name3} --start_port 2325 --start_device 3
+exp_name1="Freeway_A3C_half"
+. ./scripts/cluster.sh ./test/exp_freeway.py ${exp_name1} 4 2 --log_dir ~/hobotrl/log/Freeway/${exp_name1} --start_port 2315 --start_device 2
