dca4fdad238e1c788e6de549214c91bba8b1e6cf
RuihongQiu <qiuruihong@foxmail.com>
2018-01-12 10:25:32 +0800

    Continue to train 1e-4.

--- scripts/freeway.sh
+++ scripts/freeway.sh
@@ -2,8 +2,8 @@
 #
 #	usage: . ./scripts/iaa.sh
 #
-exp_name1="Freeway_A3C_half_3e_4"
-. ./scripts/cluster.sh ./test/exp_freeway.py ${exp_name1} 4 1 --log_dir ~/hobotrl/log/Freeway/${exp_name1} --start_port 2345 --start_device 0
+exp_name1="Freeway_A3C_half_1e_4"
+. ./scripts/cluster.sh ./test/exp_freeway.py ${exp_name1} 4 2 --log_dir ~/hobotrl/log/Freeway/${exp_name1} --start_port 2345 --start_device 2
 
 #log_dir="./log/Freeway/otdqn"
 #mkdir -p ${log_dir}
