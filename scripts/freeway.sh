34a35844e1eed23d7116000efdfdcbdcdc748986
RuihongQiu <qiuruihong@foxmail.com>
2018-01-16 11:05:15 +0800

    log dir

--- scripts/freeway.sh
+++ scripts/freeway.sh
@@ -5,7 +5,7 @@
 #exp_name3="Freeway_A3C_halfE1"
 #. ./scripts/cluster.sh ./test/exp_freeway.py ${exp_name3} 2 1 --log_dir ~/hobotrl/log/Freeway/${exp_name3} --start_port 2325 --start_device 3
 
-log_dir="./log/Freeway/OTDQNFreeway"
+log_dir="./log/Freeway/OTDQN_mom"
 mkdir -p ${log_dir}
 CUDA_VISIBLE_DEVICES=3 python test/exp_freeway.py run --name OTDQNFreeway --logdir ${log_dir} > ${log_dir}/log.txt 2>&1 &
 
