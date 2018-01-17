ca7c739ec852a2d2eb3e5ff00fc334975ed94338
RuihongQiu <qiuruihong@foxmail.com>
2018-01-17 20:14:56 +0800

    Param changes for MsPacman.

--- scripts/iaa.sh
+++ scripts/iaa.sh
@@ -5,6 +5,6 @@
 #exp_name1="Freeway_A3C_half_1e_4"
 #. ./scripts/cluster.sh ./test/exp_freeway.py ${exp_name1} 4 2 --log_dir ~/hobotrl/log/Freeway/${exp_name1} --start_port 2345 --start_device 2
 
-log_dir="./log/MsPacman/OTDQN"
+log_dir="./log/MsPacman/OTDQN_mom_0.01"
 mkdir -p ${log_dir}
 CUDA_VISIBLE_DEVICES=2 python test/exp_iaa.py run --name OTDQN --logdir ${log_dir} > ${log_dir}/log.txt 2>&1 &
