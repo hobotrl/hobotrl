029fde8e91c91755f19d20f481d333fcca849423
RuihongQiu <qiuruihong@foxmail.com>
2018-01-18 11:31:33 +0800

    Bug fix3ed

--- scripts/freeway.sh
+++ scripts/freeway.sh
@@ -7,7 +7,7 @@
 
 log_dir="./log/Freeway/OTDQN_mom_1600"
 mkdir -p ${log_dir}
-CUDA_VISIBLE_DEVICES=0 python test/exp_freeway.py run --name OTDQN_mom_1600 --logdir ${log_dir} > ${log_dir}/log.txt 2>&1 &
+CUDA_VISIBLE_DEVICES=0 python test/exp_freeway.py run --name FreewayOTDQN_mom_1600 --logdir ${log_dir} > ${log_dir}/log.txt 2>&1 &
 
 #log_dir="./log/Freeway/OTDQN_ob"
 #mkdir -p ${log_dir}
