60cc785711be19325929cd3ea95ba80261773ab8
RuihongQiu <qiuruihong@foxmail.com>
2018-01-22 17:14:39 +0800

    Exp on CarRacing new ob.

--- scripts/otdqn_drive_sim.sh
+++ scripts/otdqn_drive_sim.sh
@@ -4,7 +4,7 @@
 #
 log_dir="./log/CarRacing/OTDQN_ob_new"
 mkdir -p ${log_dir}
-CUDA_VISIBLE_DEVICES=2 python test/exp_car_flow.py run --name OTDQN_ob --render_once true --logdir ${log_dir} > ${log_dir}/log.txt 2>&1 &
+CUDA_VISIBLE_DEVICES=1 python test/exp_car_flow.py run --name OTDQN_ob --render_once true --logdir ${log_dir} > ${log_dir}/log.txt 2>&1 &
 
 #log_dir1="./log/Simul/OTDQN"
 #mkdir -p ${log_dir1}
